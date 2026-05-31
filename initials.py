095e0763e2ca50e4b1b46339c437b46bb1697d53
Joshua Ampofo <joshuaampofo37@gmail.com>
2026-05-31 04:57:08 +0000

    Initial commit

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials JA in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Joshua Ampofo
+# STUDENT ID: 2425401755
+# Email: 2425401755@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Joshua Ampofo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters
 
+    print()
+    print("...JA")
+    print()
+
+    print("...JJJJJJJJJJJJ...AAAAAAAAAAAA...****")
+    print(".......JJJJ.......AAAA....AAAA...****")
+    print(".......JJJJ.......AAAA....AAAA.......")
+    print(".......JJJJ.......AAAA....AAAA.......")
+    print(".......JJJJ.......AAAAAAAAAAAA.......")
+    print(".......JJJJ.......AAAAAAAAAAAA.......")
+    print(".......JJJJ.......AAAA....AAAA.......")
+    print(".......JJJJ.......AAAA....AAAA.......")
+    print("JJJJ...JJJJ.......AAAA....AAAA...****")
+    print("JJJJJJJJJJJ.......AAAA....AAAA...****")
+
+    print()
 
 
 main()
